6871f319e6dfd21725a49cae5ca08cf5082ec3fd
Zach Tellman <ztellman@gmail.com>
2013-06-29 10:31:01 -0700

    mark 0.1.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject byte-streams "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+(defproject byte-streams "0.1.0"
+  :description "A simple way to handle the menagerie of Java byte represenations."
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies []
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :warn-on-reflection true)
