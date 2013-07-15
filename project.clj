5f7239526f6ecfeeaf484db8f9735cb2465c7af6
Zach Tellman <ztellman@gmail.com>
2013-07-15 13:01:30 -0700

    mark 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject byte-streams "0.1.3-SNAPSHOT"
+(defproject byte-streams "0.1.3"
   :description "A simple way to handle the menagerie of Java byte represenations."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
