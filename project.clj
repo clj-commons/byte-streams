be9ace666726c33757f4c11f1e086ef025fc9599
Zach Tellman <ztellman@gmail.com>
2013-11-04 16:19:18 -0800

    mark 0.1.6

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject byte-streams "0.1.6-SNAPSHOT"
+(defproject byte-streams "0.1.6"
   :description "A simple way to handle the menagerie of Java byte represenations."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[primitive-math "0.1.3"]
-                 [clj-tuple "0.1.2"]]
+                 [clj-tuple "0.1.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [reiddraper/simple-check "0.5.0-SNAPSHOT"]
+                                  [reiddraper/simple-check "0.5.2"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
   :plugins [[codox "0.6.4"]]
   :codox {:writer codox-md.writer/write-docs
