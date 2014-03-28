22aa1d9af67806167309b5a8cc301c90a1d3a99f
Zach Tellman <ztellman@gmail.com>
2014-03-28 16:43:18 -0700

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject byte-streams "0.1.9"
+(defproject byte-streams "0.1.10-SNAPSHOT"
   :description "A simple way to handle the menagerie of Java byte represenations."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
