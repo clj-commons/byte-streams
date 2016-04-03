4797e2096de36e4c8c6326a86feb88491dd7a5b2
Zach Tellman <ztellman@gmail.com>
2016-04-02 23:30:16 -0700

    update doc generation

--- project.clj
+++ project.clj
@@ -10,8 +10,13 @@
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
   :test-selectors {:stress :stress
                    :default (complement :stress)}
-  :plugins [[codox "0.6.4"]]
-  :codox {:writer codox-md.writer/write-docs
+  :plugins [[codox "0.8.10"]
+            [lein-jammin "0.1.1"]
+            [ztellman/lein-cljfmt "0.1.10"]]
+  :cljfmt {:indents {#".*" [[:inner 0]]}}
+  :codox {:src-dir-uri "https://github.com/ztellman/byte-streams/blob/master/"
+          :src-linenum-anchor-prefix "L"
+          :defaults {:doc/format :markdown}
           :include [byte-streams]}
   :global-vars {*warn-on-reflection* true}
   :java-source-paths ["src"]
